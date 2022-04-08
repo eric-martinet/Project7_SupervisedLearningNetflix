3653e1edfcfea3cfbaee326d84e84eedf96bbed9
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-04-08 21:13:38 +0200

    Update confusion matrices

--- notebook/basic_sl.py
+++ notebook/basic_sl.py
@@ -1,5 +1,8 @@
+from locale import normalize
 import re
-from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_auc_score, roc_curve, confusion_matrix
+from turtle import color
+from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_auc_score, roc_curve, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 # SYNTHESIS FUNCTION
 def synthetise(model, X_train, X_test, y_train, y_test):
@@ -15,13 +18,18 @@ def synthetise(model, X_train, X_test, y_train, y_test):
     print(f'Recall score (macro): {recall_score(y_train, y_train_pred, average="macro"):.1%}')
     print(f'Precision score (macro): {precision_score(y_train, y_train_pred, average="macro"):.1%}')
     print(f'ROC_AUC score (macro): {roc_auc_score(y_train, y_train_score, average="macro", multi_class="ovo"):.1%}')
-    print(f'Confusion matrix')
-    print(confusion_matrix(y_train, y_train_pred))
     print('====================')
     print('TEST dataset')
     print(f'Accuracy score: {accuracy_score(y_test, y_test_pred):.1%}')
     print(f'Recall score (macro): {recall_score(y_test, y_test_pred, average="macro"):.1%}')
     print(f'Precision score (macro): {precision_score(y_test, y_test_pred, average="macro"):.1%}')
     print(f'ROC_AUC score (macro): {roc_auc_score(y_test, y_test_score, average="macro", multi_class="ovo"):.1%}')
-    print(f'Confusion matrix')
-    print(confusion_matrix(y_test, y_test_pred))
+    print(f'\n')
+
+    fig, axs = plt.subplots(1,2, figsize = (12,8))
+    ConfusionMatrixDisplay.from_predictions(y_train, y_train_pred, ax = axs[0], cmap = 'Blues', colorbar = False, normalize='all', values_format = '.1%')
+    axs[0].set_title('Confusion matrix for TRAIN dataset')
+    ConfusionMatrixDisplay.from_predictions(y_test, y_test_pred, ax = axs[1], cmap = 'Blues', colorbar = False, normalize='all', values_format = '.1%')
+    axs[1].set_title('Confusion matrix for TEST dataset')
+    fig.tight_layout()
+    fig.show()
